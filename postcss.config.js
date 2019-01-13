7727120dcd588f430671820006491a7d8eead8c2
Halvor William Sanden <halvor.william.sanden@posten.no>
2019-01-13 21:25:01 +0100

    Add footer and fine tune spacing and sizes

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
       'postcss-nested': {},
       'postcss-svg': {},
       'autoprefixer': {
+        grid: true,
         browsers: ['last 2 versions', '> 8%'],
       },
       'cssnano': {
