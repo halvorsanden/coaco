6fb3b07a9545b467dda8671edd060370bbc3821b
Halvor William Sanden <halvor.sanden@gmail.com>
2019-05-19 15:43:03 +0200

    Update html filename to match twig template

--- aftsconfig.py
+++ aftsconfig.py
@@ -1,4 +1,4 @@
 # Typically CSS, JS, favicons
 linkedFiles = ['./www/index.bundle.js', './www/style.css']
 # Typically HTML and templates
-fetcherFiles = ['./www/labsjokindex.html', './www/labsjokindex-en.html']
+fetcherFiles = ['./www/_labsjokindex.html', './www/_labsjokindex-en.html']
