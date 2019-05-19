511e9e1247c42ec2b747f8bd71aadbc261049583
Halvor William Sanden <halvor.sanden@gmail.com>
2019-05-19 20:48:00 +0200

    Change colours and spacings and remove labels

--- aftsconfig.py
+++ aftsconfig.py
@@ -1,4 +1,4 @@
 # Typically CSS, JS, favicons
-linkedFiles = ['./www/index.bundle.js', './www/style.css']
+linkedFiles = ['./www/index.bundle.js', './www/style.css', './www/assets/sjokologo.svg', './www/assets/sjokologo-en.svg']
 # Typically HTML and templates
 fetcherFiles = ['./www/_labsjokindex.html', './www/_labsjokindex-en.html']
